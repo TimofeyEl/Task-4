4b2c1110a8af02665c3dfef7d5619ecd2c6c1f68
User <User@DESKTOP-4ROMB9B>
2023-06-26 19:31:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Введите длину последовательности: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int minValue = int.MaxValue;
+            int number = 0;
+
+            for(int i = 1; i < length+1; i++)
+            {
+                Console.Write($"Введите {i} цифру: ");
+                number = Convert.ToInt32(Console.ReadLine()); 
+
+                if( minValue > number )
+                {    
+                    minValue = number;
+
+                }
+            }
+            Console.WriteLine($"Минимальное число = {minValue}");
+            Console.ReadKey();
+        }
+    }
+}
